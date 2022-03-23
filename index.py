f8007268e9651baaa7dfcea8d289ea76252c6b37
Mateus-Sousa23 <mateusmartinsipu2@gmail.com>
2022-03-23 11:52:55 -0300

    main

--- index.py
+++ index.py
@@ -4,3 +4,31 @@ connectDb = sqlite3.connect('./db/gremiovotacao.db')
 
 cursor = connectDb.cursor()
 
+cursor.execute("""
+CREATE TABLE alunos (
+        matricula VARCHAR,
+        senha VARCHAR,
+        nome VARCHAR,
+        serie    INTERGER,
+        curso VARCHAR
+);
+""")
+
+cursor.execute("""
+CREATE TABLE chapas (
+    Numero INTERGER,
+    President FK,
+    Vice FK
+)
+"""
+)
+
+cursor.execute("""
+CREATE TABLE votacao (
+    Nome_Aluno FK,
+    ID_CHAPA FK
+)
+""")
+
+connectDb.commit()
+connectDb.close()
